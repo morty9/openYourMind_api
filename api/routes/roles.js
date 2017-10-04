9f030d42b16c133928e348b71f6954f1ca93f15a
1bonp <filipjabin@yahoo.com>
2017-10-04 18:50:07 +0200

    Add routes roles.js for roles

---
+++ api/routes/roles.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+
+module.exports = (api) => {
+
+    /* --------------- GET ------------ */
+	// FindAll
+    router.get('/',
+        api.actions.notes.findAll);
+
+
+    // FindOne
+    router.get('/:id',
+        api.actions.notes.findOne);
+
+    /* ------------- POST ----------------*/
+    // Create
+    router.post('/',
+        api.middlewares.bodyParser.json(),
+        // api.middlewares.encryptUserPassword,
+        api.actions.notes.create);
+
+    /* -------------- PUT -------------- */
+    // Update
+    router.put('/:id',
+        api.middlewares.bodyParser.json(),
+        // api.middlewares.isConnected,
+        // api.middlewares.encryptUserPassword,
+        api.actions.notes.update);
+
+    /* ------------ DELETE --------------- */
+    // Delete
+    router.delete('/:id',
+        // api.middlewares.isConnected,
+        api.actions.notes.remove);
+
+    return router;
+};
