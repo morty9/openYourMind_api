11c8392a53ed5e374f68410772ae556bdfd5d39f
1bonp <filipjabin@yahoo.com>
2017-10-04 17:43:51 +0200

    Add models

---
+++ api/models/Schools.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+
+module.exports = (api) => {
+    return api.sequelize.define('Schools', {
+        id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        name: Sequelize.STRING,
+    }, {
+        timestamps: true,
+        tableName: 'user' // Forcer l'utilisation du nom de la table specifier
+    });
+}
