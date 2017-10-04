9fef181bddb6331f06bb32a388d4e54cce35a3d4
ndennu <dennu.nicolas@gmail.com>
2017-10-04 17:28:18 +0200

    crud user almost finished

---
+++ api/actions/users/findAll.js
@@ -0,0 +1,27 @@
+/*
+* Allow to find all users
+* @return User[]
+*/
+module.exports = (api) => {
+    const User = api.models.User;
+
+    return (req, res) => {
+        let respond = (users) => {
+            if(users.lenght === 0){
+                res.status(204).send(users);
+            } else {
+                res.status(200).send(users);
+            }
+        };
+
+        let returnError = (err) => {
+            res.status(500).send({ErrorCode: 500,
+                message: JSON.stringify(err)
+            });
+        };
+
+        User.findAll()
+        .then(respond)
+        .catch(returnError);
+    };
+};
---
+++ api/actions/users/remove.js
@@ -0,0 +1,38 @@
+/*
+ *  Allow to remove an user
+ */
+
+module.exports = (api) => {
+  const User = api.models.User;
+
+  return (req, res) {
+      let userId = req.params.id_user;
+
+      let isDestroyed = (data) => {
+
+          if (!data) {
+            res.status(404).send({
+              ErrorCode: 404,
+            message: 'user.not.found'});
+          }
+          res.status(200).send('User removed');
+      }
+
+        User
+        .destroy({
+          where : { id : userId }
+        })
+        .then(isDestroyed)
+        .catch(error);
+
+
+
+        function error(err) {
+          res.status(500).send({
+            ErrorCode: 500,
+            message: JSON.stringify(err)
+          });
+        }
+
+    };
+};
